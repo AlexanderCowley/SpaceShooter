7f85ed12718a6919c13c6d16a55d2bb043c325a3
Alec <movieornasasim@gmail.com>
2024-03-02 09:54:22 -0600

    Prevents enemies from bumping into each other using navmesh

--- Assets/_Project/Scripts/Characters/Enemies/AI/BehaviorTreeTest/TestBehaviorTree.cs
+++ Assets/_Project/Scripts/Characters/Enemies/AI/BehaviorTreeTest/TestBehaviorTree.cs
@@ -1,18 +1,27 @@
 using UnityEngine;
 using System.Collections.Generic;
 using BehaviorTree;
+using UnityEngine.AI;
 public class TestBehaviorTree : BehaviorTree.Tree
 {
     [SerializeField] Transform _playerTransform;
     //Replace with Stat Holder Component
     [SerializeField] float Range;
+    [SerializeField] TestBehaviorTree _testEnemy;
+    NavMeshAgent _agent;
+    Quaternion initialRotation;
+    void Awake() 
+    {
+        initialRotation = transform.rotation;
+        _agent = GetComponent<NavMeshAgent>();
+    }
     protected override Node SetUpTree()
     {
         Node root = new SequenceNode(new List<Node>()
         {
             new AttackNode(this.transform),
             new OutOfRange(this.transform, _playerTransform, Range),
-            new FollowPlayerNode(this.transform, Range),
+            new FollowPlayerNode(this.transform, _agent, Range)
         });
         return root;
     }
